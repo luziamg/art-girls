d65b8c558250f3ef259287d871ce7e3547489497
tinamayswinkels <admin@admins-MacBook-Air.local>
2017-05-12 10:42:21 +0800

    after all the menu jquery

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 // Our Code Goes Here
+
+  $(".menubutton").click(function(){
+    $(".menu").fadeToggle();
+    $(".stage-right").toggleClass("enter-stage-right");
+    $(".stage-left").toggleClass("enter-stage-left");
+    $(".stage-bottom").toggleClass('enter-stage-bottom');
+    $(".navigation").toggleClass("menubackground");
+    $(".menubutton span").toggleClass("hidden");
+    $(".topicons a").toggleClass("lighttext");
+
+    })
+
  });
